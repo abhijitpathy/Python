172ddb86230e36c474d6b5632a921edd8077133a
abhijitpathy <abhijit.pathy.cg@gmail.com>
2026-08-22 17:18:26 +0530

    variables assignment added

---
+++ Python_02/variable_assignment.py
@@ -0,0 +1,37 @@
+# Question 24
+name = "Abhi"
+age = 18
+city = "Berhampur"
+print(name,age,city)
+
+# Question 25
+student_name="Abhi"
+student_roll_number=112398
+student_branch="coumputer science"
+print(student_name,student_roll_number,student_branch)
+
+#question 26
+marks=23
+marks=25
+print(marks)
+
+#question 27
+name,age,city="rahul",18,"patna"
+print(name,age,city)
+
+#question 28
+x=y=z=0
+print(x,y,z)
+
+#question 29
+student="rahul"
+student_name="rahul"
+_class="B.tech"
+print(student,student_name,_class)
+
+#question 30
+# Student info
+student_name="Abhi"
+student_age=18
+student_age=19
+print(student_name,student_age)
