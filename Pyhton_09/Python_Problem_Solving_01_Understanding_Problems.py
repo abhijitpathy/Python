7f0f4dd640b3d72db11abc1a538338e7e55a1839
abhijitpathy <abhijit.pathy.cg@gmail.com>
2026-09-09 18:59:54 +0530

    loops and python problem assignment added

---
+++ Pyhton_09/Python_Problem_Solving_01_Understanding_Problems.py
@@ -0,0 +1,136 @@
+# #problem 1
+# num1 = int(input("enter number :"))
+# num2 = int(input("enter number :"))
+# if num1>num2:
+#     print("num1 is greater than num2")
+# else :
+#     print("num2 is greater than num1")
+
+# #problem 2
+# marks = int(input("enter marks :"))
+# if marks >=90 and marks<100 :
+#     print("A")
+# elif marks >=75 and marks<=89:
+#     print("B")
+# elif marks >=50 and marks<=74:
+#     print("C")
+# elif marks <50 and marks>=0 :
+#     print("fail")
+# else :
+#     print("invalid marks !!")
+
+# #problem 3
+# l = int(input("enter lenght :"))
+# b = int(input("enter width :"))
+# area = l * b 
+# print(area)
+# perimeter = 2*l + 2*b
+# print(perimeter)
+
+#problem 4
+
+# num = int(input("enter a number :"))
+# if num>0 :
+#     print("positive")
+# elif num<0 :
+#     print("negative")
+# else:
+#     print("number is zero")
+
+
+# #problem 5
+# price1 = int(input("enter the price of item :"))
+# if price1 >=1000:
+#     discount_percentage = 10
+#     discount = price1 * 10/100
+#     final_price = price1 - discount 
+#     print(final_price)
+# else :
+#     print("item price is less than 1000")
+
+#problem 6
+num1 = int(input("enter number 1 :"))
+num2 = int(input("enter number 2 :"))
+sum = num1 + num2
+print(sum)
+
+
+# problem 7
+num = int(input("enter number 1 :"))
+if num%2 == 0:
+    print("num is even")
+elif num%2 == 1:
+    print("num is odd")
+else:
+    print("no is zero")
+
+#problem 8
+num1 = int(input("enter number 1 :"))
+num2 = int(input("enter number 2 :"))
+num3 = int(input("enter number 3 :"))
+if num1>num2 and num1>num3 :
+    print("num1 is greater")
+elif num2>num1 and num2>num3:
+    print("num2 is greatest")
+elif num3>num1 and num3>num2 :
+    print("num3 is greatest")
+else :
+    print("all no are same")
+
+#problem 9
+age = int(input("enetr your age :"))
+if age>=18 :
+    print("eligible to vote")
+else :
+    print("not eligible to vote")
+
+#problem 10
+price1 = int(input("enter the price of item :"))
+if price1 >=2000:
+    discount_percentage = 20
+    discount = price1 * 20/100
+    final_price = price1 - discount 
+    print(final_price)
+else :
+    print("item price is less than 2000")
+
+#problem 11
+#25
+marks1 = int(input("enter first subject marks :"))
+marks2 = int(input("enter second subject marks :"))
+marks3 = int(input("enter third subject marks : "))
+#for marks1
+if marks1>= 0 and marks1<=100:
+    print("correct values")
+    if marks1>=40:
+      print("pass in subject 1")
+    elif marks1<40:
+      print("fail in subject 1 ")
+else:
+   print("invalid values")
+#for marks2
+if marks2>= 0 and marks2<=100:
+    print(" correct values")
+    if marks2>=40:
+      print("pass in subject 2")
+    elif marks2<40:
+      print("fail in subject 2 ")
+else:
+   print("invalid values")
+# for marks3
+if marks3>= 0 and marks3<=100:
+    print(" correct values")
+    if marks1>=40:
+      print("pass in subject 3")
+    elif marks1<40:
+      print("fail in subject 3 ")
+else:
+   print("invalid values")
+#for calculating average
+average = (marks1 + marks2 + marks3)/3
+if average >=40:
+   print("pass")
+elif average >100:
+   print("invalid")
+else:
+   print("you are failed")
---
+++ python_10/for_loops_assignment.py
@@ -0,0 +1,226 @@
+# #1Q printing hello
+for i in range(5):
+    print("Hello")
+
+# #2Q printing no upto 9
+for i in range(0,10):
+    print(i)
+
+# #3Q printing no from 0 to 10
+for i in range(1,11):
+    print(i)
+
+# #4Q printing in reverse
+for i in range(10,0,-1):
+    print(i)
+
+#5Q the numbers from 5 to 50, increasing by 5.
+for i in range(5,55,5):
+    print(i,end=" ")
+
+#6Q  even numbers from 2 to 20 using range().
+for i in range(1,21):
+    if i % 2 ==0:
+        print(f"{i} is even,",end=" ")
+
+#7Q all odd numbers from 1 to 19 using range()
+for i in range(1,20):
+    if i % 2 ==1:
+        print(f"{i} is odd,",end=" ")
+
+#8Q 3 6 9 12 15 18 using range()
+for i in range(3,20,3):
+    print(i,end=" ")
+
+#9Q the numbers from 20 down to 2, decreasing by 
+for i in range(20,0,-2):
+    print(i,end=" ")
+
+#10Q a positive integer n from the user and print all numbers from 1 to n.
+n = int(input("enter  number :"))
+for i in range(1,n+1):
+    print(i,end=" ")
+
+#11Q  Take n from the user and print only the even numbers from 1 to n.
+n = int(input("enter  number :"))
+for i in range(1,n+1):
+    if i %2 ==0:
+        print(f"{i} is even,",end=" ")
+
+#12Q  Take n from the user and print only the odd numbers from 1 to n.
+n = int(input("enter  number :"))
+for i in range(1,n+1):
+    if i %2 ==1:
+        print(f"{i} is odd,",end=" ")
+
+ #13Q Take n from the user and print all numbers from 1 to n that are divisible by 3.
+n = int(input("enter  number :"))
+for i in range(1,n+1):
+    if i % 3 ==0:
+        print(f"{i} is divisible by 3,",end=" ")
+
+#14Q Take n from the user and print all numbers from 1 to n that are divisible by both 2 and 3
+n = int(input("enter  number :"))
+for i in range(1,n+1):
+    if i %2 ==0 and i % 3 == 0:
+        print(f"{i} is divisible by both 2 & 3,",end=" ")
+
+# #15Q Take n from the user and count how many numbers from 1 to n are even
+n = int(input("enter number :"))
+count = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        count += 1
+
+print("Number of even numbers:", count)
+
+# # 16q 1 + 2 + 3 + ... + n
+ #using a for loop.
+n = int(input("Enter n: "))
+sum = 0
+for i in range(1, n + 1):
+    sum += i
+print("Sum =", sum)
+
+# #17q take n from the user and calculate the sum of all even numbers from 1 to n
+n = int(input("Enter num: "))
+sum_even = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        sum_even += i
+print("Sum of even numbers =", sum_even)
+
+# #18q Take n from the user and calculate the sum of all odd numbers from 1 to n
+n = int(input("Enter num: "))
+sum_odd = 0
+
+for i in range(1, n + 1):
+    if i % 2 == 1:
+        sum_odd += i
+
+print("Sum of odd numbers =", sum_odd)
+
+#19q Take a number from the user and print its multiplication table from 1 to 10
+n = int(input("enter number:"))
+for i in range(1,11):
+    print(n*i,end=" ")
+
+#20q 1 × 2 × 3 × ... × n
+ # using a for loop.
+n = int(input("Enter n: "))
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+
+print("Product =", factorial)
+
+# #21Q take a string from the user and print each character on a separate line
+l = input("enter characters :")
+for character in l:
+    print(character)
+
+# #22Q Take a string from the user and print all its characters on the same line using end=""
+l = input("enter characters :")
+for character in l:
+    print(character,end=" ")
+
+# #23Q Take a string from the user and count the number of characters in it using a for loop
+word = input("enter characters:")
+count = 0
+for character in word:
+    count = count + 1
+
+print("Characters:", count)
+
+# #24Q Take a string from the user and count how many times the character "a" appears
+word = input("enter characters :")
+count = 0
+for character in word:
+    if character == "a":
+        count = count + 1
+
+print("Count:", count)
+
+# #25Q Take a string from the user and count how many characters are uppercase letter
+text = input("Enter a string: ")
+count = 0
+for character in text:
+    if character.isupper():
+        count += 1
+
+print("Number of uppercase letters =", count)
+
+# #26 
+# Use nested loops to print:
+
+# ****
+# ****
+# ****
+for row in range(3):
+    for column in range(4):
+        print("*", end="")
+    print()
+
+# #27Q 
+# Use nested loops to print:
+
+# *****
+# *****
+# *****
+# *****
+for row in range(4):
+    for coloumn in range(4):
+        print("*",end="")
+    print()
+
+# #28Q
+28.
+# Print the following pattern:
+
+# *
+# **
+# ***
+# ****
+# *****
+for row in range(1,6):
+    for column in range(1,row+1):
+        print("*",end="")
+    print()
+
+# #29Q 
+# Print the following pattern:
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+for row in range(1,6):
+    for column in range(1,row+1):
+        print(column,end="")
+    print()
+
+
+# #30Q Create a multiplication-table grid using nested for loops.
+#For example, for numbers 1 to 5, produce rows showing their multiplication results.
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i * j, end=" ")
+    print()
+
+
+# #final one 
+# Take a number n from the user and print:
+
+# 1
+# 12
+# 123
+# 1234
+# ...
+# until the last row contains n numbers.
+n = int(input("enter number :"))
+for row in range(1,n):
+    for column in range(1,row+1):
+        print(column,end="")
+    print()
