f03b826c53fa2079edfa514174fb49199975f0be
abhijitpathy <abhijit.pathy.cg@gmail.com>
2026-09-26 09:42:57 +0530

    updated assingment

---
+++ python_solving_Assignment/02.Nested_For_Loop_Assignment/Nested_For_Loop_Assignment.py
@@ -0,0 +1,189 @@
+#q1
+# n = int(input("enter number:"))
+# for i in range (1,n+1):
+#     for j in range(1,n+1):
+#         print("*",end=" ")
+#     print()   
+
+#q2
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j,end="")
+#     print()
+
+#q3
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end="")
+#     print()
+
+
+#q4
+# n =int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end="")
+#     print()
+
+#q5
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+
+#q6
+# n = int(input("enter number :"))
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+#q7
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+
+
+# #q8
+# n =int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i*j,end=" ")
+#     print()
+
+# #q9
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,5):
+#         print(i*j,end="")
+#     print()
+
+# #q10
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j*j,end="")
+#     print()
+
+
+# #q11
+# letters = "ABCDE"
+
+# for i in range(5):
+#     for j in range(i + 1):
+#         print(letters[j], end=" ")
+#     print()
+
+# #q12
+# letters = "ABCDE"
+
+# for i in range(5):
+#     for j in range(i + 1):
+#         print(letters[i], end=" ")
+#     print()
+
+# #q13
+# n = int(input("enter number :"))
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(2 * j - 1, end=" ")
+#     print()
+
+# #q14
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(2*j,end=" ")
+#     print()
+
+# #q15
+# n = int(input('enter number :'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print("*",end=" ")
+#     print()
+
+#q16
+# #q15
+# n = int(input('enter number :'))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j,end=" ")
+#     print()
+
+# #q17
+# n= int(input("enter number :"))
+# num = 1
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(num, end=" ")
+#         num += 1
+#     print()
+
+#q18
+# n= int(input("enter number :"))
+# num = 1
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(num, end=" ")
+#         num += 1
+#     print()
+
+# #q19
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print((i,j),end=" ")
+#     print()
+
+# #q20
+# n = int(input("enter number :"))
+
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         print(i, j,)
+
+# #q21
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i*j,end=" ")
+#     print()
+
+# #q22
+# n = int(input("enter number :"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+#q23
+# n = int(input("enter number :"))
+# for i in range(n,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+#q24
+# n = int(input("enter number :"))
+# for i in range(n, 0, -1):
+#     for j in range(n, n - i, -1):
+#         print(j, end="")
+#     print()
+
+#q25
+n = int(input("enter number :"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i,end="")
+    print()
